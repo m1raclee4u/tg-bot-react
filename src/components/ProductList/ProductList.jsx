e475208e1c5236e50c379a9af990c3a6d9a8c243
m1raclee4u <64864409+m1raclee4u@users.noreply.github.com>
2023-01-26 04:38:04 +0300

    ip

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://77.244.221.114:8080/web-data/', {
+        fetch('http://77.244.221.114:8080/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
